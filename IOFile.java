91b1150f049788ac4f2473d9add247c337023d72
Marcos Adriano <marcosadsj@gmail.com>
2017-09-29 20:46:38 -0300

    IOFIle
    Done class to create, write and read files.

---
+++ IOFile.java
@@ -0,0 +1,59 @@
+public class IOFile
+{
+    private final String fileName = "Example.txt";
+    private OutputStreamWriter file = null;
+
+    public IOFile(String fileName)
+    {
+    	this.fileName = fileName;
+    }
+
+    public void createFile()
+    {
+    	try{
+        	file = new OutputStreamWriter(openFileOutput(file_name, Context.MODE_PRIVATE));
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+    	}
+	}
+
+    public void writeFile(String text) {
+
+        try{
+            file.write(text);
+            file.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+	}
+
+    public String readFile() {
+        String result = "";
+        String currentLineFile;
+
+        try {
+            InputStream file = openFileInput(file_name);
+
+            if (file != null) {
+                InputStreamReader reader = new InputStreamReader(file);
+
+                BufferedReader buff = new BufferedReader(reader);
+
+                while ((currentLineFile = buff.readLine()) != null) {
+                    result += currentLineFile;
+                }
+                file.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
