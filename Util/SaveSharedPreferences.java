385babbd60a97c733d9bd8c96516df10552976e5
Marcos Adriano <marcosadsj@gmail.com>
2017-09-30 00:34:49 -0300

    Util folder and SharedPreferences
    A new folder to improve the file oganization. Add the SaveSharedPreferences.

---
+++ Util/SaveSharedPreferences.java
@@ -0,0 +1,19 @@
+public class SaveSharedPreferences{
+
+	String fileName = "Example";
+	SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+	public SaveSharedPreferences(String fileName){
+
+		this.fileName = fileName;
+
+		sharedPreferences = getSharedPreferences(fileName, 0);
+        editor = sharedPreferences.edit();
+	}
+
+	public WriteTextToCommit(String key, String text){
+		editor.putString(key, text);
+        editor.commit();
+	}
+}
