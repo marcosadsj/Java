628bfae5a10fa343e501b4f7c269d31c14ce2e80
Marcos Adriano <marcosadsj@gmail.com>
2017-11-23 01:22:51 -0200

    Atualizando identação

--- Util/SaveSharedPreferences.java
+++ Util/SaveSharedPreferences.java
@@ -2,18 +2,18 @@ public class SaveSharedPreferences{
 
 	String fileName = "Example";
 	SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
+    	SharedPreferences.Editor editor;
 
 	public SaveSharedPreferences(String fileName){
 
 		this.fileName = fileName;
 
 		sharedPreferences = getSharedPreferences(fileName, 0);
-    	editor = sharedPreferences.edit();
+    		editor = sharedPreferences.edit();
 	}
 
 	public WriteTextToCommit(String key, String text){
 		editor.putString(key, text);
-    	editor.commit();
+    		editor.commit();
 	}
 }
